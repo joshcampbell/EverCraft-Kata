c9666eda7717ae565fec00cc7d7193f58f1c86be
Josh Campbell <campbelj@gmail.com>
2016-03-27 09:45:20 -0500

    Add attack test w/mocking
    - Git still really wants me to commit my pyc files, even though I added
      *.pyc to the .gitignore.

--- python/evercraft.py
+++ python/evercraft.py
@@ -1,7 +1,12 @@
+import random
+
 ALIGNMENTS = [ "Lawful", "Neutral", "Chaotic" ]
 
 class InvalidAlignmentException(Exception):
   pass
+
+def roll(sides):
+  return random.randint(1,sides)
 
 class Character:
 
@@ -9,6 +14,7 @@ class Character:
     self.name("Anonymous")
     self.alignment("Neutral")
     self.armor_class(10)
+    self.hit_points(5)
 
   def name(self, new_name=None):
     if new_name != None:
@@ -27,3 +33,16 @@ class Character:
     if new_ac != None:
       self._armor_class = new_ac
     return self._armor_class
+
+  def hit_points(self, new_hp=None):
+    if new_hp != None:
+      self._hit_points = new_hp
+    return self._hit_points
+
+  def attack(self, other):
+    result = roll(20)
+    if result >= other.armor_class():
+      other.damage(1)
+
+  def damage(self, hp_amount):
+    self.hit_points(self.hit_points() - hp_amount)
--- python/tests/test_character.py
+++ python/tests/test_character.py
@@ -1,11 +1,21 @@
 import unittest
 
-from evercraft import Character, InvalidAlignmentException, ALIGNMENTS
+from mock import MagicMock
+from mock import patch
 
-class TestCharacter(unittest.TestCase):
+from evercraft import Character, InvalidAlignmentException, ALIGNMENTS
+from evercraft import roll
+
+# NOTE: Look at all of these redundant property tests! Can we factor this logic
+#       into a mixin or something? Aren't Python's annotations meant to handle
+#       cases like this?
+
+class DefaultCharacter(unittest.TestCase):
 
     def setUp(self):
         self.character = Character()
+
+class TestName(DefaultCharacter):
 
     def test_name_property(self):
         self.assertEqual(self.character.name(),"Anonymous")
@@ -14,6 +24,8 @@ class TestCharacter(unittest.TestCase):
         new_name = "Baron Harkonnen"
         self.character.name(new_name)
         self.assertEqual(self.character.name(), new_name)
+
+class TestAlignment(DefaultCharacter):
 
     def test_alignment_property(self):
         self.assertEqual(self.character.alignment(),"Neutral")
@@ -28,6 +40,8 @@ class TestCharacter(unittest.TestCase):
                           self.character.alignment,
                           "Accelerationist")
 
+class TestArmorClass(DefaultCharacter):
+
     def test_armor_class_property(self):
         self.assertEqual(self.character.armor_class(),10)
 
@@ -35,8 +49,36 @@ class TestCharacter(unittest.TestCase):
         self.character.armor_class(15) 
         self.assertEqual(self.character.armor_class(),15)
 
-# NOTE: Look at all of these redundant property tests! Can we factor this logic
-#       into a mixin or something? Aren't Python's annotations meant to handle
-#       cases like this?
+class TestHitPoints(DefaultCharacter):
 
-# TODO: Combat test cases will require mocking. Need to read about it first.
+    def test_hit_points_property(self):
+        self.assertEqual(self.character.hit_points(),5)
+
+    def test_change_hit_points_property(self):
+        self.character.hit_points(1000)
+        self.assertEqual(self.character.hit_points(),1000)
+
+class TestAttacking(unittest.TestCase):
+
+    def setUp(self):
+        self.character = Character()
+        self.opponent = Character()
+        self.opponent.damage = MagicMock()
+
+    @patch("evercraft.roll", return_value=5)
+    def test_attacking_rolls_a_d20(self,roll):
+        self.character.attack(self.opponent)
+        self.assertEqual(roll.call_count, 1)
+
+    @patch("evercraft.roll", return_value=19)
+    def test_hits_when_rolling_at_or_over_target_ac(self, roll):
+        self.character.attack(self.opponent)
+        self.assertEqual(self.opponent.damage.call_count, 1)
+
+    @patch("evercraft.roll", return_value=2)
+    def test_miss_when_rolling_below_target_ac(self, roll):
+        self.character.attack(self.opponent)
+        self.assertEqual(self.opponent.damage.call_count, 0)
+
+class TestBeingHit(DefaultCharacter):
+    pass
